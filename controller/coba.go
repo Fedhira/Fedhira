1ed975db3960329b9b6a7dc3a4ce1e19686f3850
Fedhira Syaila <pedirasyaila@gmail.com>
2023-03-31 14:48:35 +0700

    latihan6

--- controller/coba.go
+++ controller/coba.go
@@ -3,12 +3,53 @@ package controller
 import(
 	inimodel "github.com/Fedhira/Tagihan/model"
 	inimodul "github.com/Fedhira/Tagihan/module"
+	inimodullatihan "github.com/indrariksa/be_presensi/module"
 	"github.com/aiteung/musik"
 	cek "github.com/aiteung/presensi"
 	"github.com/gofiber/fiber/v2"
 	"github.com/Fedhira/Tugas_1214028/config"
 	"net/http"
+	"errors"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func GetAllPresensi(c *fiber.Ctx) error {
+	ps := inimodullatihan.GetAllPresensi(config.Ulbimongoconn, "presensi")
+	return c.JSON(ps)
+}
+
+func GetPresensiID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": "Wrong parameter",
+		})
+	}
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": "Invalid id parameter",
+		})
+	}
+	ps, err := inimodullatihan.GetPresensiFromID(objID, config.Ulbimongoconn, "presensi")
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"status":  http.StatusNotFound,
+				"message": fmt.Sprintf("No data found for id %s", id),
+			})
+		}
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": fmt.Sprintf("Error retrieving data for id %s", id),
+		})
+	}
+	return c.JSON(ps)
+}
 
 func Home(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
--- url/url.go
+++ url/url.go
@@ -11,7 +11,7 @@ func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
 	page.Get("/", controller.Homepage) //ujicoba panggil package musik
-	page.Get("/presensi", controller.GetPresensi)
+	page.Get("/presen", controller.GetPresensi)
 	page.Get("/nasabah", controller.GetNasabah)
 	page.Get("/penagih", controller.GetPenagih)
 	page.Get("/tagihan", controller.GetTagihan)
@@ -21,5 +21,8 @@ func Web(page *fiber.App) {
 	page.Post("/inspenagih", controller.InsertPenagih)
 	page.Post("/instagihan", controller.InsertTagihan)
 	page.Post("/insbank", controller.InsertBank)
+
+	page.Get("/presensi", controller.GetAllPresensi) //menampilkan seluruh data presensi
+	page.Get("/presensi/:id", controller.GetPresensiID) //menampilkan data presensi berdasarkan id
 
 }
