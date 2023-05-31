588c69ba1f13cd0c3eb562624ca408f4a37190e6
Fedhira Syaila <pedirasyaila@gmail.com>
2023-05-31 14:19:10 +0700

    latihan11

--- main.go
+++ main.go
@@ -18,6 +18,17 @@ import (
 	_ "github.com/Fedhira/Tugas_1214028/docs"
 )
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+
 func main() {
 	go whatsauth.RunHub()
 	site := fiber.New(config.Iteung)
